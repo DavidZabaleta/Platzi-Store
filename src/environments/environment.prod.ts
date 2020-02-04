2934a0bcb78b4143a48661441f7ed5fed3333617
David Zabaleta <davidzabaletaf@gmail.com>
2020-02-03 21:10:41 -0500

    No changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  urlApi: 'http://platzi-store.herokuapp.com/',
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  urlApi: 'http://platzi-store.herokuapp.com/',
   production: false
 };
 
