4d91019062374dbe9184578370e1d3992e31b8bc
David Zabaleta <david.zabaleta@sofka.com.co>
2020-02-04 10:55:04 -0500

    Files added

---
+++ src/app/admin/components/edit-product/edit-product.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ProductsService } from 'src/app/core/service/products.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { MyValidators } from 'src/app/utils/validators';
+
+@Component({
+  selector: 'app-edit-product',
+  templateUrl: './edit-product.component.html',
+  styleUrls: ['./edit-product.component.scss']
+})
+export class EditProductComponent implements OnInit {
+
+  form: FormGroup;
+  id: string;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private productsService: ProductsService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.buildForm();
+   }
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      this.id = params.id;
+      this.productsService.getProduct(this.id)
+      .subscribe(product => {
+        this.form.patchValue(product);
+      });
+    });
+  }
+
+  private buildForm() {
+    this.form = this.formBuilder.group({
+      title: ['', [Validators.required]],
+      price: [0, [Validators.required, MyValidators.isPriceValid]],
+      image: [''],
+      description: ['', [Validators.required]]
+    });
+  }
+
+  saveProduct(event: Event) {
+    event.preventDefault();
+    if (this.form.valid) {
+      const product = this.form.value;
+      this.productsService.updateProduct(this.id, product)
+      .subscribe(() => {
+        this.router.navigate(['./admin/products']);
+      });
+    }
+  }
+
+  get priceField() {
+    return this.form.get('price');
+  }
+
+}
